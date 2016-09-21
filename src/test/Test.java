cf75d885cd0ea0da59b16fc61c1dc8ca955a824b
林嘉豪 <1143910315@qq.com>
2016-09-21 13:41:53 +0800

    新寻路框架
    剪枝寻路

--- src/test/Test.java
+++ src/test/Test.java
@@ -12,10 +12,10 @@ public class Test {
 		 * aJFrame.setVisible(true);
 		 */
 		System.out.println(System.currentTimeMillis());
-		int left = 10000;
-		int top = 10000;
+		int left = 2000;
+		int top = 2000;
 		Xunlu aXunlu = new Xunlu(left, top);
-		aXunlu.setorigin(0, 0);
+		aXunlu.setorigin(left / 2, top / 2);
 		aXunlu.xunlu(left - 1, top - 1);
 		System.out.println(System.currentTimeMillis());
 	}
